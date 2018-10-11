8d73daf37c9f9980536ed63dc5d9df6f2c81c145
Kobayashi <necrophantasia_koba@icloud.com>
2018-10-11 10:25:03 +0900

    Audio

--- K3D12/Engine/Audio/IAudio.h
+++ K3D12/Engine/Audio/IAudio.h
@@ -1,16 +1,41 @@
 #pragma once
 
+#include <xaudio2.h>
+
+#include <string>
+
 namespace K3D12 {
 	class IAudio
 	{
+		friend class AudioManager;
+		friend class AudioLoader;
 	private:
+
+	protected:
+
+		unsigned int _loopPoint;
+
+		bool _isPlay;
+
+		std::string _soundName;
+
 	public:
+
 	private:
+
 	public:
 
 		IAudio();
 
 		virtual ~IAudio();
+
+		void Stop();
+
+		void Play();
+
+		void SetLoopPoint(unsigned int frame);
+
+		bool IsPlay();
 	};
 }
 
